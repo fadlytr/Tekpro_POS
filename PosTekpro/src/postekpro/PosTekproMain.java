44c039d62a1bdaf32a436a71b7093919ead84c86
HamParasUt <hamzah.prasetio.tif417@polban.ac.id>
2018-04-25 16:13:00 +0700

    penambahan read dari json
    penambahan read dari json

--- PosTekpro/src/postekpro/PosTekproMain.java
+++ PosTekpro/src/postekpro/PosTekproMain.java
@@ -5,6 +5,11 @@
  */
 package postekpro;
 
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  *
  * @author Ahmad_Aji_Naufal_Ali
@@ -14,8 +19,11 @@ public class PosTekproMain {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
+        Gson gson = new Gson(); 
+        BufferedReader br = new BufferedReader(new FileReader("*/../maps.json"));
+        Barang target = gson.fromJson(br, Barang.class);
     }
     
 }
