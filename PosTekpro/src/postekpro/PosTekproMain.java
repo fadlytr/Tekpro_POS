8663547cd1612b0a2b062b53829920dc0276269f
naufala <ahmadajinaufalali@gmail.com>
2018-05-01 15:00:11 +0700

    tambah class pembayaran

--- PosTekpro/src/postekpro/PosTekproMain.java
+++ PosTekpro/src/postekpro/PosTekproMain.java
@@ -25,13 +25,17 @@ public class PosTekproMain {
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
-        Gson gson = new Gson(); 
+        Gson gson = new Gson();
         BufferedReader br = new BufferedReader(new FileReader("src/json/ListBarang3.json"));
         Barang target = gson.fromJson(br, Barang.class);
+        
+        BufferedReader rek = new BufferedReader(new FileReader("src/json/ListRekening2.json"));
+        Rekening target_2 = gson.fromJson(rek, Barang.class);
+        
         Scanner baca = new Scanner(System.in);
         int filter;
-        
-        do{
+
+        do {
             System.out.println("1. print semua barang");
             System.out.println("2. print Minuman");
             System.out.println("3. print Makanan");
@@ -41,37 +45,32 @@ public class PosTekproMain {
             System.out.println("7. print Makanan Ringan\n");
             System.out.print("pilih menu filter: ");
             filter = baca.nextInt();
-        }while(filter<1 || filter>7);
-        
+        } while (filter < 1 || filter > 7);
+
         //List <Produk> barang = target.getAllData();
-        List <Produk> barang = target.filterData(filter);
+        List<Produk> barang = target.filterData(filter);
         Scanner scan = new Scanner(System.in);
-        
-        
+
         Iterator<Produk> n = barang.iterator();
-        while(n.hasNext())
-        {
+        while (n.hasNext()) {
             Produk z = n.next();
-            System.out.print("\n"+z.getId().getCode() + " " + z.getId().getNama() + " " + z.getHarga());
+            System.out.print("\n" + z.getId().getCode() + " " + z.getId().getNama() + " " + z.getHarga());
         }
         System.out.print("\nMasukkan kode barang : ");
-     
+
         String kode = scan.next();
-        
+
         boolean temu = false;
         n = barang.iterator();
-        while ( n.hasNext() && !temu )
-        {
+        while (n.hasNext() && !temu) {
             Produk z = n.next();
-            if (z.getId().getCode().equalsIgnoreCase(kode))
-                    {
-                        temu = true;
-                        System.out.println(z.getId().getNama() + " " + z.getHarga() );
-                    }
+            if (z.getId().getCode().equalsIgnoreCase(kode)) {
+                temu = true;
+                System.out.println(z.getId().getNama() + " " + z.getHarga());
+            }
         }
-        
-        if (temu == false) 
-        {
+
+        if (temu == false) {
             System.out.println("Kode tidak ditemukan ");
         }
     }
